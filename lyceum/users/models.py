c6583d47aa97d0bcf47202253d69bf2256f56b0a
Xenx3128 <Xenx3024@yandex.ru>
2022-12-05 19:54:00 +0500

    fix: fix user creation

--- lyceum/users/models.py
+++ lyceum/users/models.py
@@ -22,7 +22,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     objects = UserManager()
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ('name',)
+    REQUIRED_FIELDS = ('username',)
 
     class Meta:
         verbose_name = 'пользователь'
