5d9756c418e23079355c5da859d51988bc1d168b
Xenx3128 <Xenx3024@yandex.ru>
2022-12-05 15:30:02 +0500

    refactor: declare MEDIA_URL, STATIC_URL only in DEBUG mode

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -14,10 +14,12 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
 if settings.DEBUG:
     import debug_toolbar
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls))
     ]
