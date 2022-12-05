16591a832a99d04837d61d8d0db5492d2717f261
Xenx3128 <Xenx3024@yandex.ru>
2022-12-05 22:18:56 +0500

    refactor: view context fix

--- lyceum/catalog/views.py
+++ lyceum/catalog/views.py
@@ -7,7 +7,6 @@ def item_list(request):
     items = Item.objects.published_by_category()
     context = {
         'items': items,
-        'app_name': 'catalog',
     }
     return render(request, 'catalog/item_list.html', context)
 
@@ -16,6 +15,5 @@ def item_detail(request, pk):
     item = get_object_or_404(Item, pk=pk, is_published=True)
     context = {
         'item': item,
-        'app_name': 'catalog',
     }
     return render(request, 'catalog/item_detail.html', context)
--- lyceum/homepage/views.py
+++ lyceum/homepage/views.py
@@ -6,6 +6,5 @@ def home(request):
     items = Item.objects.published_main()
     context = {
         'items': items,
-        'app_name': 'homepage',
     }
     return render(request, 'homepage/index.html', context)
