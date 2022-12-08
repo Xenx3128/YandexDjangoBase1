93730d5a174c3979d5d9623b215fbe6a6b300a83
Xenx3128 <Xenx3024@yandex.ru>
2022-12-09 01:08:56 +0500

    in-dev

--- lyceum/core/context_processors/age.py
+++ lyceum/core/context_processors/age.py
@@ -6,10 +6,6 @@ from users.models import User
 def birthdays(request):
     today = datetime.date.today()
     birthdays = User.objects.filter(birthday__lte=today)
-
-    # list = [user for user in User.objects.all()
-    #         if user.birthday.strftime('%b %d') ==
-    #         datetime.date.today().strftime('%b %d')]
     return {
         'birth_users': birthdays,
     }
