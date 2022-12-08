a735e47b434775b36cd49e08e7df509bde442896
Xenx3128 <Xenx3024@yandex.ru>
2022-12-09 01:44:48 +0500

    refactor: name fix

--- lyceum/core/context_processors/age.py
+++ lyceum/core/context_processors/age.py
@@ -5,8 +5,8 @@ from users.models import User
 
 def birthdays(request):
     day, month = datetime.date.today().day, datetime.date.today().month
-    birthdays = User.objects.filter(birthday__day=day, birthday__month=month)
+    bday_users = User.objects.filter(birthday__day=day, birthday__month=month)
 
     return {
-        'birth_users': birthdays,
+        'birth_users': bday_users,
     }
