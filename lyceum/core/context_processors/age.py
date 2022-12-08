aa67be15d22cf4ffdb9db5e6f5d2d2e332e4bce0
Xenx3128 <Xenx3024@yandex.ru>
2022-12-09 00:07:15 +0500

    fix: fix context processor

--- lyceum/core/context_processors/age.py
+++ lyceum/core/context_processors/age.py
@@ -4,9 +4,13 @@ from users.models import User
 
 
 def birthdays(request):
-    list = [user for user in User.objects.all()
-            if user.birthday.strftime('%b %d') ==
-            datetime.date.today().strftime('%b %d')]
+    list = []
+    for user in User.objects.all():
+        if user.birthday:
+            birthday = user.birthday.strftime('%b %d')
+            today = datetime.date.today().strftime('%b %d')
+            if birthday == today:
+                list.append(user)
     return {
         'birth_users': list,
     }
