3538be774aaa755f770f7cf4ab51ec8cf4b6846c
Xenx3128 <Xenx3024@yandex.ru>
2022-12-08 23:38:52 +0500

    refactor: fix flake8

--- lyceum/core/context_processors/age.py
+++ lyceum/core/context_processors/age.py
@@ -1,11 +1,12 @@
-from users.models import User
 import datetime
+
+from users.models import User
 
 
 def birthdays(request):
-    list = [user for user in User.objects.all() if user.birthday.strftime('%b %d') == datetime.date.today().strftime('%b %d')]
-    print('#####################################################################################')
-    print(list)
+    list = [user for user in User.objects.all()
+            if user.birthday.strftime('%b %d') ==
+            datetime.date.today().strftime('%b %d')]
     return {
         'birth_users': list,
     }
